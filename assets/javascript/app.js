017bcd20255a8f6022496a51ec19b4e4cbc7099b
bethgranade <bgranado488@gmail.com>
2019-10-24 20:27:44 -0500

    defined var in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var ul=document.getElementById('ul');
+var btn=document.getElementById('button');
+var scoreCard=document.getElementById('scoreCard');
+var quizBox=document.getElementById('questionBox');
+var op1=document.getElementById('op1');
+var op2=document.getElementById('op2');
+var op3=document.getElementById('op3');
+var op4=document.getElementById('op4');
+
